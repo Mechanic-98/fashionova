3541453e2092d9efbe7540de00f99cf3cab69a6c
Mechanic-98 <parasherabhi98@gmail.com>
2022-07-26 01:27:39 +0530

    Email and password auth, sign up and component

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -4,6 +4,7 @@ import {
 	signInWithRedirect,
 	signInWithPopup,
 	GoogleAuthProvider,
+	createUserWithEmailAndPassword,
 } from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
@@ -18,31 +19,42 @@ const firebaseConfig = {
 
 const firebaseApp = initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider(); // GoogleAuthProvider is a class
+// we have other class as well like, facebookAuthProvider, githubAuthProvider etc
 
-provider.setCustomParameters({
+googleProvider.setCustomParameters({
 	prompt: 'select_account',
 });
 
+// auth is a singleton which keeps track of authentication of the entire app as user sign in
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
 
+export const signInWithGooglePopup = () =>
+	signInWithPopup(auth, googleProvider);
+
+// this singleton object directs points to our db in console
 export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async (userAuth) => {
-	const userDocRef = doc(db, 'users', userAuth.uid);
+export const createUserDocumentFromAuth = async (userAuth, additionalInfo) => {
+	if (!userAuth) return;
+	const userDocRef = doc(db, 'users', userAuth.uid); // doc methods allow us to retreive doc from a collection in db
+	// it receives (db, collection name, unique id of document)
+	// -> since we are storing users thus we are using user's uid as a uinique id to for doc
 
-	const userSnapshot = await getDoc(userDocRef);
+	const userSnapshot = await getDoc(userDocRef); // getDoc allow us to get data from document
 
 	if (!userSnapshot.exists()) {
 		const { displayName, email } = userAuth;
 		const createdAt = new Date();
 
 		try {
+			// setDoc allow us to set data in document
+			// it takes document reference and the data we want to set
 			await setDoc(userDocRef, {
 				displayName,
 				email,
 				createdAt,
+				...additionalInfo,
 			});
 		} catch (error) {
 			console.log('error creating the user', error.message);
@@ -50,4 +62,10 @@ export const createUserDocumentFromAuth = async (userAuth) => {
 	}
 
 	return userDocRef;
+};
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+	if (!email || !password) return;
+
+	return await createUserWithEmailAndPassword(auth, email, password);
 };
